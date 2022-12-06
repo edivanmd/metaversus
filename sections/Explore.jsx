bbe47e000dad2bb21d7e3eebd6266624547ff5e0
edivanmd <edivanmd@gmail.com>
2022-12-06 11:40:17 +1100

    Explore section

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,5 +1,13 @@
 'use client';
 
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { ExploreCard, TitleText,  TypingText } from '../components';
+
+import { staggerContainer } from '../utils/motion';
+import { exploreWolds } from '../constants';
+import styles from '../styles';
+
 const Explore = () => (
   <section>
     Explore section
