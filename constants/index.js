4750202644a2f0f5ba8ab0ac133d5e6dbe149564
edivanmd <edivanmd@gmail.com>
2022-12-06 16:41:47 +1100

    What's New section.

--- constants/index.js
+++ constants/index.js
@@ -36,14 +36,12 @@ export const newFeatures = [
   {
     imgUrl: '/vrpano.svg',
     title: 'A new world',
-    subtitle:
-        'we have the latest update with new world for you to try never mind',
+    subtitle: 'we have the latest update with new world for you to try never mind',
   },
   {
     imgUrl: '/headset.svg',
     title: 'More realistic',
-    subtitle:
-        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+    subtitle: 'In the latest update, your eyes are narrow, making the world more realistic than ever',
   },
 ];
 
@@ -51,20 +49,17 @@ export const insights = [
   {
     imgUrl: '/planet-06.png',
     title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
-    subtitle:
-        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+    subtitle: 'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
   },
   {
     imgUrl: '/planet-07.png',
     title: '7 tips to easily master the madness of the Metaverse',
-    subtitle:
-        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+    subtitle: 'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
   },
   {
     imgUrl: '/planet-08.png',
     title: 'With one platform you can explore the whole world virtually',
-    subtitle:
-        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+    subtitle: 'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
   },
 ];
 
