1513137deb0b391cdded53cddf88962da694a3be
edivanmd <edivanmd@gmail.com>
2022-12-08 19:06:18 +1100

    Changes

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,10 +1,8 @@
 'use client';
 
-import styles from '../styles';
-
 const Menu = ({ handleNavbar, name, url }) => (
   <li className="flex text-left">
-    <a onClick={handleNavbar} href={url} className="font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 text-xl md:text-4xl ease-in duration-300 hover:scale-110 hover:underline "> 
+    <a onClick={handleNavbar} href={url} className="font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 text-xl md:text-4xl ease-in duration-300 hover:scale-110 hover:underline ">
       &gt;{name}
     </a>
   </li>
